bf5fc81630ea0c5cd48fcfd05aa4b110712c55b3
GIT <nguyencafe171@gmail.com>
2023-06-07 20:23:13 +0700

    SUM and SUBTRACT

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main
 {
     public static void main(String args[])
     {
-        int n1 = 225, n2 = 115, sum;
+        int n1 = 225, n2 = 115, sum, subtract;
         sum = n1 + n2;
+        subtract= n1 - n2;
         System.out.println("The sum of numbers is: "+sum);
     }
 }
