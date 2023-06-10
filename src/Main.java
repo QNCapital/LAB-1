b96e8c7f0024b28741ad6eac2a1653492257c814
GIT <nguyencafe171@gmail.com>
2023-06-10 16:01:36 +0700

    x=3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main
 {
     public static void main(String args[])
     {
-        int n1 = 225, n2 = 115, sum, subtract;
+        int n1 = 225, n2 = 9, sum, subtract;
+        int x=3;
         sum = n1 + n2;
         subtract= n1 - n2;
         System.out.println("The sum of numbers is: "+sum);
     }
 }
+x
